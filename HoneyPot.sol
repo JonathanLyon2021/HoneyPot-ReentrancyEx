708d14c3fa790e7821550a5c114b1ba9b22227cc
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-23 11:36:02 -0400

    Third Commit
    Implemented public function/method withdraw.

--- HoneyPot.sol
+++ HoneyPot.sol
@@ -3,8 +3,17 @@ pragma solidity ^0.8.1;
 contract HoneyPot {
     // Todo: Implementation
     mapping (address => uint) public balances;
-    
-     function deposit() public payable {
+
+    function deposit() public payable {
         balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        require(baqlances[msg.sender] > 0);
+        (bool success, ) = msg.sender.call{ value: balances[msg.sender] }("");
+        if (!success){
+            revert();
+        }
+        balances[msg.sender]= 0;
     }
 }
