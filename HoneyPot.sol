3c02f7ca7e2a8147006760c44f2553ee2eef0f82
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-23 11:18:59 -0400

    Second Commit
    Added public payable function/method deposit(). function. This function stores the ether value sent to the contract. Note
    that msg.sender here is the address that broadcasted the transaction. Line 7-9.

--- HoneyPot.sol
+++ HoneyPot.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.1;
 contract HoneyPot {
     // Todo: Implementation
     mapping (address => uint) public balances;
+    
+     function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
 }
